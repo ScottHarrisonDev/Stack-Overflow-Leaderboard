d92da7dedcf43296f110581dc0fcc5bd653e8cd8
Scott Harrison <github@scottharrisondesign.co.uk>
2014-11-24 23:37:21 +0000

    Initial Commit
    Adding all files

---
+++ js/main.js
@@ -0,0 +1,69 @@
+tskStack = {
+
+	init: function(){
+		var self = this;
+		self.fnGetUserData('709625;1879628;368846;1283391;309956;3387421');
+		self.fnEasterEgg();
+	},
+
+	fnGetUserData: function(userIDs){
+		var self = this;
+		self.fnDoAPICall(userIDs);		
+	},
+
+	fnDoAPICall: function(userIDs){
+		var self = this;
+		$.ajax({
+			url: "https://api.stackexchange.com/2.2/users/" + userIDs + "?key=kPQWcAQVhj3ex1b0XR1Mng((&site=stackoverflow&order=desc&sort=reputation&filter=default",
+			success: function(data){
+				self.fnClearBoard();
+				$.each(data.items,function(index,value){
+					self.fnAddUser(value);
+				});
+				self.fnAddTolocalStorage(data);				
+			},
+			error: function(data){
+				var storedUserData = localStorage['userData'] || 'empty';
+				if (storedUserData == 'empty'){
+					alert('Something went really wrong...');
+				} else {
+					var parsedStoredUserData = JSON.parse(storedUserData);
+					$.each(parsedStoredUserData.items,function(index,value){
+						self.fnAddUser(value);
+					});
+				}
+			}
+		});
+	},
+
+	fnAddTolocalStorage: function(data){
+		var self = this;
+		localStorage['userData'] = JSON.stringify(data);
+	},
+
+	fnClearBoard: function(){
+		var self = this;
+		$('#leaderboardWrapper').empty();
+	},
+
+	fnAddUser: function(userData){
+		var self = this;
+		var szUserHTML = '<a href="' + userData.link + '" target="_blank"><div class="row userRow"><img class="userImg" src="' + userData.profile_image + '" alt="Avatar"><div class="userInfo"><h4 class="userName">' + userData.display_name + '</h4><p class="userReputation">' + userData.reputation + '</p><div class="badges"><span class="bronze">' + userData.badge_counts.bronze + '</span><span class="silver">' + userData.badge_counts.silver + '</span><span class="gold">' + userData.badge_counts.gold + '</div></div></div></div></div></a>';
+		$('#leaderboardWrapper').append(szUserHTML);
+	},
+
+	fnEasterEgg: function(){
+		var self = this;
+		$('#easterEgg').mouseover(function(){
+			$(this).addClass('fa-spin');
+		});
+		$('#easterEgg').mouseout(function(){
+			$(this).removeClass('fa-spin');
+		});
+	}
+
+}
+
+$(document).ready(function(){
+	tskStack.init();
+});
